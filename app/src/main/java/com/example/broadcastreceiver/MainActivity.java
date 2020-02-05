543d1b39afe7132429dffe789ca21f7a2733f9d9
Salman <salmansabir2@gmail.com>
2020-02-05 20:05:23 +0500

    5/2/2020

---
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.broadcastreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        registerReceiver(broadcastReceiver , new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            int level = intent.getIntExtra("level" , 0);
+            ProgressBar progressBar = findViewById(R.id.progressBar);
+            progressBar.setProgress(level);
+            TextView textView = findViewById(R.id.txt_view);
+            textView.setText(" Battery Level" + Integer.toString(level)+"%");
+
+        }
+    };
+}
